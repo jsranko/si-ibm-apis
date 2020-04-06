4df8c373f3d622fffef5ba3c304bec90b0714882
Juraj Sranko <jus@sranko-informatik.de>
2020-04-06 10:31:44 -0400

    SICRTMSF changed

--- si-ibmi-apis/QCMDSRC/SICRTMSGF.cmd
+++ si-ibmi-apis/QCMDSRC/SICRTMSGF.cmd
@@ -1,3 +1,3 @@
-             CMD        PROMPT('Create Messagefile from streamfile')
+             CMD        PROMPT('(SI) Create Messagefile')
 
              PARM       KWD(SRCSTMF) TYPE(*CHAR) LEN(256) CASE(*MIXED) PROMPT('Streamfile')
